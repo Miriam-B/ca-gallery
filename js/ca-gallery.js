d7055bbecaec58407a98cff02d85933fbd429951
Yaron Mushinsky <maryam1649@gmail.com>
2021-04-07 17:50:29 +0300

    fixed some more ugly stuff

--- js/ca-gallery.js
+++ js/ca-gallery.js
@@ -2,7 +2,7 @@ var gProjs = [{
   "id": "ExPacman",
   "name": "Pacman",
   "title": "Eat the food and avoid your enemies",
-  "desc": "This game is just like the original pacman, the only",
+  "desc": "This game is just like the original Pacman,<br>the only difference is that it doesn't quite look like it.",
   "url": "projs/ExPacman/index.html",
   "publishedAt": 1448693940000,
   "labels": ["Matrixes", "keyboard events"]
@@ -11,7 +11,7 @@ var gProjs = [{
   "id": "ExMinesweeper",
   "name": "Minesweeper",
   "title": "Walk on the field and avoid getting bombed",
-  "desc": "later",
+  "desc": "The beloved Minesweeper game,<br>just prettier + you cannot lose in the begginer difficulty level.",
   "url": "projs/ExMinesweeper/index.html",
   "publishedAt": 1448693940000,
   "labels": ["Matrixes", "keyboard events"]
@@ -20,7 +20,7 @@ var gProjs = [{
   "id": "ExInPicture",
   "name": "In Picture",
   "title": "What's in the picture? choose wisely",
-  "desc": "later",
+  "desc": "This game is simple.<br>You see a picture and choose the correct answer of what's in that picture.",
   "url": "projs/ExInPicture/index.html",
   "publishedAt": 1448693940000,
   "labels": ["Matrixes", "keyboard events"]
@@ -78,7 +78,7 @@ var gProjs = [{
             <i class="fa fa-plus fa-3x"></i>
           </div>
         </div>
-        <img class="img-fluid" src="img/portfolio/${project.id}.jpg" alt="No image to show :(">
+        <img class="img-fluid game-pic" src="img/portfolio/${project.id}.jpg" alt="No image to show :(">
       </a>
       <div class="portfolio-caption">
         <h4>${project.name}</h4>
